cf25a378f07a3a0c4d78cdaab081fc3c93a7dfcb
Oladejo Jephthah <jephthahooh@gmail.com>
2025-03-15 20:22:19 +0100

    login moved to auth

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -5,11 +5,14 @@ use App\Http\Controllers\Api\AuthController;
 
 // use App\Http\Controllers\Api\AdminController;
 
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    // Route::post('/register', [AuthController::class, 'register']);
+    // Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+});
 
-Route::post('/login', [AuthController::class, 'login']);
 
-// Route::post('/register', [AuthController::class, 'register']);
-// Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
 // Route::middleware(['auth:sanctum', 'admin'])->group(function () {
 //     Route::get('/admin/dashboard', [AdminController::class, 'dashboard']);
 // });
